d288393980d399a2f6f5742b7242dd3d31fe59e0
jmsmg <jmsmg1@me.com>
2023-04-25 12:18:37 +0900

    개편

--- get_next_line.c
+++ get_next_line.c
@@ -6,76 +6,79 @@
 /*   By: seonggoc <seonggoc@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 14:29:08 by seonggoc          #+#    #+#             */
-/*   Updated: 2023/04/24 20:43:21 by seonggoc         ###   ########.fr       */
+/*   Updated: 2023/04/25 12:18:03 by seonggoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-char	*get_remain(char *remain)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (remain[i] && remain[i] != '\n')
-	{
-		i++;
-	}
-	j = ft_strlen(remain);
-	if (i != j)
-	{
-		remain = ft_substr(remain, i + 1, j);
-		return (remain);
-	}
-	return(remain);
-}
-
-char	*get_return_value(char *remain)
+char	*get_remain(char *ptr)
 {
 	int		i;
-	char	*ptr;
+	char	*tmp;
 
 	i = 0;
-	while (remain[i] && remain[i] != '\n' )
+	while (ptr[i] && ptr[i] != '\n' )
 	{
 		i++;
 	}
-	ptr = ft_substr(remain, 0, i + 1);
-	return (ptr);
+	if (ptr[i] == '\0' || ptr[i + 1] == '\0')
+	{
+		return (NULL);
+	}
+	tmp = ft_substr(ptr, 0, i + 1);
+	return (tmp);
+}
+
+char	*get_return_value(int fd, char *buf, char *ptr, char *remain)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 1;
+	while (len)
+	{
+		len = read(fd, buf, BUFFER_SIZE);
+		if (len == -1)
+			return (NULL);
+		else if (len == 0)
+			return (remain);
+		buf[len] = '\0';
+		ptr = remain;
+		remain = ft_strjoin(remain, buf);
+		free(ptr);
+		if (remain == NULL)
+			return (NULL);
+		while (buf[i] && buf[i] == '\n')
+			return (remain);
+	}
+	return (remain);
 }
 
 char	*get_next_line(int fd)
 {
-	int			i;
-	int			len;
 	char		buf[BUFFER_SIZE + 1];
 	char		*ptr;
 	static char	*remain;
 
-	i = 0;
-	len = 1;
-	if (fd < 3 || BUFFER_SIZE < 1)
-		return (NULL);
-	while (0 != len)
+	ptr = 0;
+	if (fd < 0 || BUFFER_SIZE < 1)
 	{
-		len = read(fd, buf, BUFFER_SIZE);
-		if (len == -1)
-			return (0);
-		if (!remain)
-			remain = ft_strdup("");
-		ptr = ft_strdup("");
-		buf[len] = '\0';
-		remain = ft_strjoin(remain, buf);
-		ptr = get_return_value(remain);
-		remain = get_remain(remain);
-		while (ptr[i])
-		{
-			if (ptr[i] == '\n')
-				return (ptr);
-			i++;
-		}
+		return (NULL);
 	}
+	if (!remain)
+	{
+		remain = ft_strdup("");
+	}
+	ptr = get_return_value(fd, buf, ptr, remain);
+	if (!ptr || ptr[0] == '\0')
+	{
+		free(remain);
+		remain = NULL;
+		return (NULL);
+	}
+	remain = get_remain(ptr);
 	return (ptr);
 }
