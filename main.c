6a56e88145fd9a327deb6921191329f06ddd7e55
jmsmg <jmsmg1@me.com>
2023-04-25 19:33:28 +0900

    fin

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: seonggoc <seonggoc@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 12:46:25 by seonggoc          #+#    #+#             */
-/*   Updated: 2023/04/25 16:53:42 by seonggoc         ###   ########.fr       */
+/*   Updated: 2023/04/25 19:25:38 by seonggoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -20,12 +20,8 @@ int	main()
 	int		fd;
 
 	fd = open("./test1.txt", O_RDONLY);
-	fd = open("./test2.txt", O_RDONLY);
 	if (fd != -1)
 	{
-
-		printf("42_with_nl : %s", get_next_line(fd));
-		printf("42_with_nl : %s", get_next_line(fd));
 
 		printf("42_with_nl : %s", get_next_line(fd));
 		printf("42_with_nl : %s", get_next_line(fd));
